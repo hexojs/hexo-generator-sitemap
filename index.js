ccd5ce2041eb2e51ab4580f7855e2e2e4e362f76
Tommy Chen <tommy351@gmail.com>
2014-07-13 00:23:14 +0800

    Add generator name

--- index.js
+++ index.js
@@ -1,35 +1,7 @@
-var ejs = require('ejs'),
-  merge = require('utils-merge'),
-  path = require('path'),
-  file = hexo.util.file2;
+var generator = hexo.extend.generator;
 
-var sitemapSrc = path.join(__dirname, 'sitemap.ejs'),
-  sitemapTmpl = ejs.compile(file.readFileSync(sitemapSrc));
-
-hexo.extend.generator.register(function(locals, render, callback){
-  var config = hexo.config;
-
-  var sitemapConfig = merge({
-    path: 'sitemap.xml'
-  }, config.sitemap);
-
-  if (!path.extname(sitemapConfig.path)){
-    sitemapConfig.path += '.xml';
-  }
-
-  var posts = [].concat(locals.posts.toArray(), locals.pages.toArray())
-    .filter(function(item){
-      return item.sitemap !== false;
-    })
-    .sort(function(a, b){
-      return b.updated - a.updated;
-    });
-
-  var xml = sitemapTmpl({
-    config: config,
-    posts: posts
-  });
-
-  hexo.route.set(sitemapConfig.path, xml);
-  callback();
-});
+if (generator.register.length === 1){
+  generator.register(require('./sitemap'));
+} else {
+  generator.register('sitemap', require('./sitemap'));
+}
