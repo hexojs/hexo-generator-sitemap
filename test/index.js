5df7fe70e588f2cea8b766ebbe69c1dd01a98ea3
weyusi <weyusi@users.noreply.github.com>
2019-05-31 12:28:33 +0930

    test: add test for page layout

--- test/index.js
+++ test/index.js
@@ -9,39 +9,49 @@ describe('Sitemap generator', function() {
   hexo.config.sitemap = {
     path: 'sitemap.xml'
   };
-  var Post = hexo.model('Post');
-  var generator = require('../lib/generator').bind(hexo);
-  var sitemapTmpl = require('../lib/template')(hexo.config);
-  var posts,
-    locals;
+  const Post = hexo.model('Post');
+  const Page = hexo.model('Page');
+  const generator = require('../lib/generator').bind(hexo);
+  const sitemapTmpl = require('../lib/template')(hexo.config);
+  let posts = {};
+  let pages = {};
+  let locals = {};
 
-  before(function() {
-    return hexo.init().then(function() {
+  before(() => {
+    return hexo.init().then(() => {
       return Post.insert([
         {source: 'foo', slug: 'foo', updated: 1e8},
-        {source: 'bar', slug: 'bar', updated: 1e8 + 1},
-        {source: 'baz', slug: 'baz', updated: 1e8 - 1}
-      ]).then(function(data) {
+        {source: 'bar', slug: 'bar', updated: 1e8 - 1},
+        {source: 'baz', slug: 'baz', updated: 1e8 - 2}
+      ]).then(() => {
+        return Page.insert([
+          {source: 'bio/index.md', path: 'bio/', updated: 1e8 - 3},
+          {source: 'about/index.md', path: 'about/', updated: 1e8 - 4}
+        ])
+      }).then(() => {
         posts = Post.sort('-updated');
+        pages = Page.sort('-update');
         locals = hexo.locals.toObject();
       });
     });
   });
 
-  it('default', function() {
-    var result = generator(locals);
+  it('default', () => {
+    const result = generator(locals);
 
     result.path.should.eql('sitemap.xml');
     result.data.should.eql(sitemapTmpl.render({
       config: hexo.config,
       posts: posts.toArray()
+      .concat(pages.toArray())
     }));
 
-    var $ = cheerio.load(result.data);
+    const $ = cheerio.load(result.data);
+    const allPosts = Object.assign({}, posts.data.concat(pages.data))
 
-    $('urlset').find('url').each(function(i) {
-      $(this).children('loc').text().should.eql(posts.eq(i).permalink);
-      $(this).children('lastmod').text().should.eql(posts.eq(i).updated.toISOString());
+    $('url').each((index, element) => {
+      $(element).children('loc').text().should.eql(allPosts[index].permalink);
+      $(element).children('lastmod').text().should.eql(allPosts[index].updated.toISOString());
     });
   });
 
