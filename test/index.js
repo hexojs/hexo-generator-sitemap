7f3810bd54284b07d89eca68ed67d6b1f71716cc
curbengh <43627182+curbengh@users.noreply.github.com>
2019-08-10 21:24:53 +0930

    test: add test for pages

--- test/index.js
+++ test/index.js
@@ -10,9 +10,11 @@ describe('Sitemap generator', () => {
     path: 'sitemap.xml'
   };
   const Post = hexo.model('Post');
+  const Page = hexo.model('Page');
   const generator = require('../lib/generator').bind(hexo);
   const sitemapTmpl = require('../lib/template')(hexo.config);
   let posts = {};
+  let pages = {};
   let locals = {};
 
   before(() => {
@@ -23,6 +25,14 @@ describe('Sitemap generator', () => {
         {source: 'baz', slug: 'baz', updated: 1e8 - 1}
       ]).then(data => {
         posts = data.sort((a, b) => b.updated - a.updated);
+      }).then(() => {
+        return Page.insert([
+          {source: 'bio/index.md', path: 'bio/', updated: 1e8 - 3},
+          {source: 'about/index.md', path: 'about/', updated: 1e8 - 4}
+        ]);
+      }).then(data => {
+        posts = Post.sort('-updated');
+        pages = Page.sort('-update');
         locals = hexo.locals.toObject();
       });
     });
@@ -34,14 +44,15 @@ describe('Sitemap generator', () => {
     result.path.should.eql('sitemap.xml');
     result.data.should.eql(sitemapTmpl.render({
       config: hexo.config,
-      posts: posts
+      posts: posts.toArray().concat(pages.toArray())
     }));
 
     const $ = cheerio.load(result.data);
+    const allPosts = Object.assign({}, posts.data.concat(pages.data));
 
     $('url').each((index, element) => {
-      $(element).children('loc').text().should.eql(posts[index].permalink);
-      $(element).children('lastmod').text().should.eql(posts[index].updated.toISOString());
+      $(element).children('loc').text().should.eql(allPosts[index].permalink);
+      $(element).children('lastmod').text().should.eql(allPosts[index].updated.toISOString());
     });
   });
 
