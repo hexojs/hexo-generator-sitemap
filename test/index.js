2ecf3661d25779fb5ec8b6ddc976e06b58c45062
curbengh <43627182+curbengh@users.noreply.github.com>
2019-09-09 11:37:34 +0100

    test: refactor to work around contexts

--- test/index.js
+++ test/index.js
@@ -1,18 +1,65 @@
 'use strict';
 
-const should = require('chai').should(); // eslint-disable-line
 const Hexo = require('hexo');
+const should = require('chai').should(); // eslint-disable-line
 const cheerio = require('cheerio');
+const { encodeURL } = require('hexo-util');
 
-describe('Sitemap generator', () => {
+const ctx = {
+  config: {
+    sitemap: {
+      path: 'sitemap.xml'
+    }
+  }
+};
+
+const generator = require('../lib/generator').bind(ctx);
+
+describe('generator', () => {
   const hexo = new Hexo(__dirname, {silent: true});
-  hexo.config.sitemap = {
-    path: 'sitemap.xml'
-  };
   const Post = hexo.model('Post');
-  const generator = require('../lib/generator').bind(hexo);
-  const sitemapTmpl = require('../lib/template')(hexo.config);
   let posts = {};
+  let locals = {};
+
+  before(() => {
+    return hexo.init();
+  });
+
+  it('should generate', () => {
+    Post.insert([
+      {source: 'foo', slug: 'foo', updated: 1e8},
+      {source: 'bar', slug: 'bar', updated: 1e8 + 1},
+      {source: 'baz', slug: 'baz', updated: 1e8 - 1}
+    ]).then(data => {
+      posts = data.sort((a, b) => b.updated - a.updated);
+      locals = hexo.locals.toObject();
+
+      const sitemapTmpl = require('../lib/template')(ctx.config);
+      const result = generator(locals);
+
+      result.path.should.eql('sitemap.xml');
+      result.data.should.eql(sitemapTmpl.render({
+        config: ctx.config,
+        posts: posts
+      }));
+    });
+  });
+
+  it('should sort in descending order', () => {
+    const result = generator(locals);
+
+    const $ = cheerio.load(result.data);
+
+    $('url').each((index, element) => {
+      $(element).children('loc').text().should.eql(posts[index].permalink);
+      $(element).children('lastmod').text().should.eql(posts[index].updated.toISOString());
+    });
+  });
+});
+
+describe('skip_render', () => {
+  const hexo = new Hexo(__dirname, {silent: true});
+  const Post = hexo.model('Post');
   let locals = {};
 
   before(() => {
@@ -22,30 +69,12 @@ describe('Sitemap generator', () => {
         {source: 'bar', slug: 'bar', updated: 1e8 + 1},
         {source: 'baz', slug: 'baz', updated: 1e8 - 1}
       ]).then(data => {
-        posts = Post.sort('-updated');
         locals = hexo.locals.toObject();
       });
     });
   });
 
-  it('default', () => {
-    const result = generator(locals);
-
-    result.path.should.eql('sitemap.xml');
-    result.data.should.eql(sitemapTmpl.render({
-      config: hexo.config,
-      posts: posts.toArray()
-    }));
-
-    const $ = cheerio.load(result.data);
-
-    $('url').each((index, element) => {
-      $(element).children('loc').text().should.eql(posts.eq(index).permalink);
-      $(element).children('lastmod').text().should.eql(posts.eq(index).updated.toISOString());
-    });
-  });
-
-  describe('skip_render', () => {
+  describe('should skip file', () => {
     it('array', () => {
       hexo.config.skip_render = ['foo'];
 
@@ -65,6 +94,33 @@ describe('Sitemap generator', () => {
 
       const result = generator(locals);
       result.should.be.ok;
+    });
+  });
+});
+
+describe('IDN', () => {
+  const hexo = new Hexo(__dirname, {silent: true});
+  const Post = hexo.model('Post');
+
+  before(() => {
+    hexo.config.url = 'http://fôo.com/bár';
+    return hexo.init();
+  });
+
+  it('should encode URL properly', () => {
+    const parsedUrl = encodeURL(hexo.config.url);
+    Post.insert([
+      {source: 'foo', slug: 'foo', updated: 1e8},
+      {source: 'bar', slug: 'bar', updated: 1e8 + 1},
+      {source: 'baz', slug: 'baz', updated: 1e8 - 1}
+    ]).then(data => {
+      const result = generator(hexo.locals.toObject());
+
+      const $ = cheerio.load(result.data);
+
+      $('url').each((index, element) => {
+        $(element).children('loc').text().startsWith(parsedUrl).should.be.true;
+      });
     });
   });
 });
