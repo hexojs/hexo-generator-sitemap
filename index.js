c5f7c55a0e5f601751ad7a47c599f105d9c1cd42
curbengh <43627182+curbengh@users.noreply.github.com>
2019-09-11 15:43:07 +0930

    refactor: no unused argument, destructure path (#69)
    * refactor(test): no unused argument
    
    * style(test): chai should
    
    * refactor: destructure path module
    
    * refactor: destructure fs module

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 /* global hexo */
 'use strict';
 
-const pathFn = require('path');
+const { extname } = require('path');
 
 const config = hexo.config.sitemap = Object.assign({
   path: 'sitemap.xml'
 }, hexo.config.sitemap);
 
-if (!pathFn.extname(config.path)) {
+if (!extname(config.path)) {
   config.path += '.xml';
 }
 
