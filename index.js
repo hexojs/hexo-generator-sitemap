39ef767dd9270ef5d792eeae4b970a118594765a
curbengh <43627182+curbengh@users.noreply.github.com>
2019-09-24 11:26:24 +0100

    fix: disable rel-sitemap by default

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { extname } = require('path');
 
 const config = hexo.config.sitemap = Object.assign({
   path: 'sitemap.xml',
-  rel: true
+  rel: false
 }, hexo.config.sitemap);
 
 if (!extname(config.path)) {
