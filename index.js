f6453680cdc936b8d8526f7a23e1ab25dcc7d013
uiolee <22849383+uiolee@users.noreply.github.com>
2022-02-12 15:48:27 +0800

    add generate `sitemap.txt` default generate sitemap.txt, just use `config.sitemap.txt` to disable. ``` sitemap:     txt: false ```

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 
 const { extname } = require('path');
 
+const genger = require('./lib/generator');
 hexo.config.sitemap = Object.assign({
   path: 'sitemap.xml',
   rel: false,
   tags: true,
-  categories: true
+  categories: true,
+  txt: true
 }, hexo.config.sitemap);
 
 const config = hexo.config.sitemap;
@@ -16,7 +18,10 @@ if (!extname(config.path)) {
   config.path += '.xml';
 }
 
-hexo.extend.generator.register('sitemap', require('./lib/generator'));
+hexo.extend.generator.register("sitemap.xml", locals => {return genger.call(hexo, locals, config.path);});
+if(config.txt==true){
+  hexo.extend.generator.register("sitemap.txt", locals => {return genger.call(hexo, locals, "sitemap.txt");});
+}
 
 if (config.rel === true) {
   hexo.extend.filter.register('after_render:html', require('./lib/rel'));
