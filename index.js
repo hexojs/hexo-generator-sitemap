8d4dc5b8b90c2527eef00ac714ba08224bd726c2
uiolee <22849383+uiolee@users.noreply.github.com>
2022-02-13 11:50:23 +0800

    lints fix

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ if (!extname(config.path)) {
   config.path += '.xml';
 }
 
-hexo.extend.generator.register("sitemap.xml", locals => {return genger.call(hexo, locals, config.path);});
-if(config.txt==true){
-  hexo.extend.generator.register("sitemap.txt", locals => {return genger.call(hexo, locals, "sitemap.txt");});
+hexo.extend.generator.register('sitemap.xml', locals => { return genger.call(hexo, locals, config.path); });
+if (config.txt === true) {
+  hexo.extend.generator.register('sitemap.txt', locals => { return genger.call(hexo, locals, 'sitemap.txt'); });
 }
 
 if (config.rel === true) {
